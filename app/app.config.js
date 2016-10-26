d30e84036f9c0983a9ff1e37cba6376129e788e1
iisha small <iishasmall@iishas-MacBook-Pro.local>
2016-10-25 20:21:36 -0400

    added routing

---
+++ app/app.config.js
@@ -0,0 +1,20 @@
+angular.module('recipeApp').config(
+    
+    function config($locationProvider, $routeProvider){
+        $locationProvider.hashPrefix('!');
+        $routeProvider.
+        when('/',{
+
+            template:'<recipe-list></recipe-list>'
+          }).
+        when('/recipes/:recipeId',{
+
+            template:'<recipe-detail></recipe-detail>'
+        }).
+        otherwise('/recipes');
+
+    }
+    
+    
+    
+    )
